a328acfbdc95fde245881a850e4bb574a32b5f79
Dmytro-Melnyshyn <Dmytro_Melnyshyn@epam.com>
2021-09-20 10:45:36 +0300

    add tests to ManageConribution flow

---
+++ src/settings/components/ManageContribution/ManageContributionView.test.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { renderWithIntl } from '@folio/stripes-data-transfer-components/test/jest/helpers';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import { translationsProperties } from '../../../../test/jest/helpers';
+import ManageContributionView from './ManageContributionView';
+
+jest.mock('@folio/stripes-components', () => ({
+  ...jest.requireActual('@folio/stripes-components'),
+  Tooltip: jest.fn(() => <div>Tooltip</div>),
+}));
+
+const serverOptions = [
+  {
+    id: 'f8723a94-25d5-4f19-9043-cc3c306d54a1',
+    label: 'CSC',
+    value: 'f8723a94-25d5-4f19-9043-cc3c306d54a1'
+  },
+  {
+    id: '6e2b3e23-8d58-4cd3-985d-b4eb2e9a2ec9',
+    label: 'testName2',
+    value: '6e2b3e23-8d58-4cd3-985d-b4eb2e9a2ec9'
+  }
+];
+
+const selectedServerMock = {
+  id: serverOptions[1].id,
+  name: serverOptions[1].label,
+};
+
+const currentContribution = {
+  itemTypeMappingStatus: "Valid",
+  locationsMappingStatus: "Invalid",
+  status: "Not started",
+};
+
+const renderCentralPatronTypeForm = ({
+  selectedServer = selectedServerMock,
+  currentContributionHistory = [],
+  currentContributionHistoryCount = 0,
+  isCurrentContributionPending = false,
+  isCurrentContributionHistoryPending = false,
+  showContributionHistory = false,
+  selectContributionHistory,
+  selectCurrentContribution,
+  onChangeServer,
+  onInitiateContribution,
+  onNeedMoreContributionHistoryData,
+} = {}) => {
+  return renderWithIntl(
+    <Router history={createMemoryHistory()}>
+      <ManageContributionView
+        currentContribution={currentContribution}
+        currentContributionHistory={currentContributionHistory}
+        currentContributionHistoryCount={currentContributionHistoryCount}
+        isCurrentContributionPending={isCurrentContributionPending}
+        isCurrentContributionHistoryPending={isCurrentContributionHistoryPending}
+        showContributionHistory={showContributionHistory}
+        selectedServer={selectedServer}
+        serverOptions={serverOptions}
+        selectContributionHistory={selectContributionHistory}
+        selectCurrentContribution={selectCurrentContribution}
+        onChangeServer={onChangeServer}
+        onInitiateContribution={onInitiateContribution}
+        onNeedMoreContributionHistoryData={onNeedMoreContributionHistoryData}
+      />
+    </Router>,
+    translationsProperties,
+  );
+};
+
+describe('ManageContributionView', () => {
+  const onChangeServer = jest.fn();
+  const selectContributionHistory = jest.fn();
+  const selectCurrentContribution = jest.fn();
+  const onInitiateContribution = jest.fn();
+  const onNeedMoreContributionHistoryData = jest.fn();
+
+
+  const commonProps = {
+    onChangeServer,
+    selectContributionHistory,
+    selectCurrentContribution,
+    onInitiateContribution,
+    onNeedMoreContributionHistoryData,
+  };
+
+  it('should be rendered', () => {
+    const { container } = renderCentralPatronTypeForm(commonProps);
+
+    expect(container).toBeVisible();
+  });
+});
---
+++ src/settings/routes/ManageContributionRoute/ManageContribution.test.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import {
+  cloneDeep,
+} from 'lodash';
+import { screen, act } from '@testing-library/react';
+
+import { renderWithIntl } from '@folio/stripes-data-transfer-components/test/jest/helpers';
+import { createMemoryHistory } from 'history';
+import { translationsProperties } from '../../../../test/jest/helpers';
+import ManageContributionRoute from './ManageContribution';
+import ManageContributionView from '../../components/ManageContribution/ManageContributionView';
+import { useCentralServers } from '../../../hooks';
+
+jest.mock('../../components/ManageContribution/ManageContributionView', () => {
+  return jest.fn(() => <div>ManageContributionView</div>);
+});
+
+jest.mock('@folio/stripes-components', () => ({
+  LoadingPane: jest.fn(() => <div>LoadingPane</div>),
+}));
+
+jest.mock('../../../hooks', () => ({
+  ...jest.requireActual('../../../hooks'),
+  useCentralServers: jest.fn().mockReturnValue({}),
+}));
+
+const servers = [
+  {
+    id: '1',
+    name: 'testServerName1',
+  },
+  {
+    id: '2',
+    name: 'testServerName2',
+  },
+];
+
+const serverOptions = [
+  {
+    id: '1',
+    value: 'testServerName1',
+    label: 'testServerName1',
+  },
+  {
+    id: '2',
+    value: 'testServerName2',
+    label: 'testServerName2',
+  },
+];
+
+const resourcesMock = {
+  centralServerRecords: {
+    records: [{ centralServers: servers }],
+    isPending: false,
+    hasLoaded: false,
+  },
+};
+
+const mutatorMock = {
+  selectedServerId: {
+    replace: jest.fn(),
+  },
+  currentContribution: {
+    GET: jest.fn(() => Promise.resolve()),
+  },
+  contributionHistory: {
+    GET: jest.fn(() => Promise.resolve()),
+  },
+  initiateContribution: {
+    GET: jest.fn(() => Promise.resolve()),
+  },
+};
+
+const renderManageContributionRoute = ({
+  resources = resourcesMock,
+  mutator = mutatorMock,
+} = {}) => {
+  return renderWithIntl(
+    <ManageContributionRoute
+      resources={resources}
+      mutator={mutator}
+      history={createMemoryHistory()}
+    />,
+    translationsProperties,
+  );
+};
+
+describe('renderManageContributionRoute component', () => {
+  const selectedServer = servers[1];
+  const handleServerChange = jest.fn();
+
+  beforeEach(() => {
+    ManageContributionView.mockClear();
+    useCentralServers.mockClear().mockReturnValue({
+      selectedServer,
+      serverOptions,
+      handleServerChange,
+    });
+  });
+
+  it('should be rendered', async () => {
+    let component;
+    await act(async () => {
+      component = renderManageContributionRoute();
+    });
+
+    expect(component).toBeDefined();
+  });
+
+  it('should display loading', async () => {
+    const newResources = cloneDeep(resourcesMock);
+
+    newResources.centralServerRecords.isPending = true;
+    await act(async () => {
+      renderManageContributionRoute({ resources: newResources });
+    });
+    expect(screen.getByText('LoadingPane')).toBeVisible();
+  });
+
+  describe('handleServerChange function', () => {
+    beforeEach(async () => {
+      renderManageContributionRoute();
+      await act(async () => { await ManageContributionView.mock.calls[1][0].onChangeServer(servers[1].name); });
+    });
+
+    it('should change the selected server in redux', () => {
+      expect(mutatorMock.selectedServerId.replace).toHaveBeenCalled();
+    });
+
+    it('should pass the selected server', () => {
+      expect(ManageContributionView.mock.calls[3][0].selectedServer).toEqual(servers[1]);
+    });
+  });
+});
